3999acc8ae98048bd8c85a760b4894f47f3e6196
subong2013@163.com <subong2013@163.com>
2019-04-09 23:03:43 +0800

    home:第一次

--- app/src/main/java/com/starpine/gittest/MainActivity.java
+++ app/src/main/java/com/starpine/gittest/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //更新
         String a = "ge";
+        //加多几个
+        String b = "jgaj";
     }
 }
